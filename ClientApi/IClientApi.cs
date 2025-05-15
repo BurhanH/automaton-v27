5269d18ed8ea11eab30a04ad6c117cf9263ea7a2
Baur Urazalinov <BurhanH@users.noreply.github.com>
2025-05-15 13:54:17 -0500

    Added Client library

---
+++ ClientApi/IClientApi.cs
@@ -0,0 +1,23 @@
+﻿using Refit;
+
+namespace ClientApi;
+
+public interface IClientApi
+{
+    public const string BaseUrl = "https://jsonplaceholder.typicode.com";
+    
+    [Get("/posts")]
+    Task<HttpResponseMessage>GetPosts();
+    
+    [Get("/posts/{id}")]
+    Task<HttpResponseMessage>GetPost(int id);
+    
+    [Get("/posts/{id}/comments")]
+    Task<HttpResponseMessage>GetPostComments(int id);
+    
+    [Patch("/posts/{id}")]
+    Task<HttpResponseMessage>PatchPost(int id);
+    
+    [Delete("/posts/{id}")]
+    Task<HttpResponseMessage>DeletePost(int id);
+}
